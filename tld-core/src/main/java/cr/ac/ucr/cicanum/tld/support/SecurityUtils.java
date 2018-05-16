4d9fc892c15f96e6be8b038b3475f8d479efe4c6
Fabian Roberto Leandro <roleandroflores12@gmail.com>
2018-05-16 16:49:48 -0600

    Spring Security
    Setup Password Encoder Bean with a random value
    Added SecurityUtils.java to handle password validation

---
+++ tld-core/src/main/java/cr/ac/ucr/cicanum/tld/support/SecurityUtils.java
@@ -0,0 +1,50 @@
+package cr.ac.ucr.cicanum.tld.support;
+
+/**
+ * Class which provides security utils such as validating a user's password.
+ *
+ * @author Josué León Sarkis
+ */
+public class SecurityUtils {
+
+    /**
+     * Method which validades a user's password by checking it is at least 8 characters long,
+     * it has at least one: number, upper case letter, lower case letter and symbol.
+     * @param password The user's password to validate
+     */
+    public static void validatePassword(String password) {
+
+        boolean passwordTooShort = password.length() < 8;
+        if (passwordTooShort)
+            throw new IllegalArgumentException("Invalid password, the password should at least have 8 characters.");
+
+        boolean noNumber = true;
+        boolean noUpperCaseLetter = true;
+        boolean noLowerCaseLetter = true;
+        boolean noSymbol = true;
+
+        for (int i = 0; i < password.length(); i++) {
+            char c = password.charAt(i);
+            if (noNumber)
+                noNumber = !(c >= 48 && c <= 57);
+            if (noUpperCaseLetter)
+                noUpperCaseLetter = !(c >= 65 && c <= 90);
+            if (noLowerCaseLetter)
+                noLowerCaseLetter = !(c >= 97 && c <= 122);
+            if (noSymbol)
+                noSymbol = !((c >= 33 && c <= 47) || (c >= 58 && c <= 64));
+
+            if (!(noNumber || noUpperCaseLetter || noLowerCaseLetter || noSymbol)) break;
+        }
+
+        if (noNumber)
+            throw new IllegalArgumentException("Invalid password, the password should at least have a number.");
+        if (noUpperCaseLetter)
+            throw new IllegalArgumentException("Invalid password, the password should at least have an upper case letter.");
+        if (noLowerCaseLetter)
+            throw new IllegalArgumentException("Invalid password, the password should at least have a lower case letter.");
+        if (noSymbol)
+            throw new IllegalArgumentException("Invalid password, the password should at least have a symbol.");
+    }
+
+}
