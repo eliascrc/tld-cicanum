797f79d59b5a72f19227d25f3790f6bc9c897775
Fabian Roberto Leandro <roleandroflores12@gmail.com>
2018-05-29 10:34:14 -0600

    Updated password validation to meet business requirements.

--- tld-core/src/main/java/cr/ac/ucr/cicanum/tld/support/SecurityUtils.java
+++ tld-core/src/main/java/cr/ac/ucr/cicanum/tld/support/SecurityUtils.java
@@ -7,45 +7,37 @@ package cr.ac.ucr.cicanum.tld.support;
  */
 public class SecurityUtils {
 
-    // TODO Preguntarle a Jonatan sobre los requerimientos de la contraseña
     /**
-     * Method which validades a user's password by checking it is at least 8 characters long,
-     * it has at least one: number, upper case letter, lower case letter and symbol.
+     * Method which validades a user's password by checking it is at least 5 characters long,
+     * it has at least one: number, letter and symbol.
      * @param password The user's password to validate
      */
     public static void validatePassword(String password) {
 
-        boolean passwordTooShort = password.length() < 8;
-        if (passwordTooShort)
-            throw new IllegalArgumentException("Invalid password, the password should at least have 8 characters.");
+        if (password.length() < 5)
+            throw new IllegalArgumentException("Invalid password, the password should at least have 5 characters.");
 
         boolean noNumber = true;
-        boolean noUpperCaseLetter = true;
-        boolean noLowerCaseLetter = true;
+        boolean noLetter = true;
         boolean noSymbol = true;
 
         for (int i = 0; i < password.length(); i++) {
             char c = password.charAt(i);
             if (noNumber)
                 noNumber = !(c >= 48 && c <= 57);
-            if (noUpperCaseLetter)
-                noUpperCaseLetter = !(c >= 65 && c <= 90);
-            if (noLowerCaseLetter)
-                noLowerCaseLetter = !(c >= 97 && c <= 122);
+            if (noLetter)
+                noLetter = !((c >= 65 && c <= 90) || (c >= 97 && c <= 122));
             if (noSymbol)
                 noSymbol = !((c >= 33 && c <= 47) || (c >= 58 && c <= 64));
-
-            if (!(noNumber || noUpperCaseLetter || noLowerCaseLetter || noSymbol)) break;
+            if (!(noNumber || noLetter || noSymbol)) break;
         }
 
         if (noNumber)
-            throw new IllegalArgumentException("Invalid password, the password should at least have a number.");
-        if (noUpperCaseLetter)
-            throw new IllegalArgumentException("Invalid password, the password should at least have an upper case letter.");
-        if (noLowerCaseLetter)
-            throw new IllegalArgumentException("Invalid password, the password should at least have a lower case letter.");
+            throw new IllegalArgumentException("Invalid password, the password should at least have one number.");
         if (noSymbol)
-            throw new IllegalArgumentException("Invalid password, the password should at least have a symbol.");
+            throw new IllegalArgumentException("Invalid password, the password should at least have one symbol.");
+        if (noLetter)
+            throw new IllegalArgumentException("Invalid password, the password should at least have one letter.");
     }
 
 }
